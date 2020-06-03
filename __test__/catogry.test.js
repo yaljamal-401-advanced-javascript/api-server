69274bbb5d399faac5263e238318950b713d4b03
yaljamal <yazan.aljamal1@gmail.com>
2020-06-03 22:49:42 +0300

    modify lab9

--- __test__/catogry.test.js
+++ __test__/catogry.test.js
@@ -6,42 +6,42 @@ const obj = {
   description: 'electronices',
   display_name: 'electronices',
 };
-describe('catogry Modle ',()=>{
-  it('create ()',()=>{
-    return catogry.create(obj).then((result)=>{
-      Object.keys(obj).forEach((keys)=>{
+describe('catogry Modle ', () => {
+  it('create ()', () => {
+    return catogry.create(obj).then((result) => {
+      Object.keys(obj).forEach((keys) => {
         expect(result[keys]).toEqual(obj[keys]);
       });
     });
   });
-  it('get ()',()=>{
-    return catogry.get().then((result)=>{
-      Object.keys(obj).forEach((keys)=>{
+  it('get ()', () => {
+    return catogry.get().then((result) => {
+      Object.keys(obj).forEach((keys) => {
         expect(result[0][keys]).toEqual(obj[keys]);
       });
     });
   });
-  it('get(id)',()=>{
-    return catogry.get(obj.id).then((result)=>{
-      Object.keys(obj).forEach((keys)=>{
+  it('get(id)', () => {
+    return catogry.get(obj.id).then((result) => {
+      Object.keys(obj).forEach((keys) => {
         expect(result[0][keys]).toEqual(obj[keys]);
       });
     });
   });
-  //   it('put(id)',()=>{
-  //     let newObj={
-  //       category: 'electronices',
-  //       name: 'smart phone',
-  //       display_name: 'iphone 11',
-  //       description: '512 GB',
-  //     };
-  //     return catogry.get(obj.id).then((result)=>{
-  //       catogry.update(result._id,newObj).then(record=>{
-  //         Object.keys(record).forEach((keys)=>{
-  //           expect(newObj[0][keys]).toEqual(result[keys]);
-  //         });
+  // it('put(id)', () => {
+  //   let newObj = {
+  //     category: 'electronices',
+  //     name: 'smart phone',
+  //     display_name: 'iphone 11',
+  //     description: '512 GB',
+  //   };
+  //   return catogry.get(obj.id).then((result) => {
+  //     return catogry.update(result._id, newObj).then(record => {
+  //       Object.keys(newObj).forEach((keys) => {
+  //         expect(newObj[0][keys]).toEqual(obj[keys]);
   //       });
   //     });
   //   });
+  // });
 
 });
---
+++ routes/router.js
@@ -0,0 +1,91 @@
+'use strict';
+const express = require('express');
+const products = require('../models/produtes-model.js');
+const categories = require('../models/catogry-model.js');
+const router = express.Router();
+
+router.param('model', getModel);
+
+function getModel(req, res, next) {
+  const model = req.params.model;
+  switch (model) {
+  case 'categories':
+    req.model=categories;
+    next();
+    return;     
+  case 'products':
+    req.model=products;
+    next();
+    return;
+
+  default:
+    next('Invaled Model');
+    return;
+  }
+}
+
+//-----------------Routs-------------------------------
+router.get('/:model',getHandler);
+router.get('/:model/:id',getByIdHandler);
+router.post('/:model',postHandler);
+router.put('/:model/:id',updateByIdHandler);
+router.delete('/:model/:id',deleteHandler);
+
+//-----------------getHandler-------------------------------
+
+async function getHandler(req,res,next) { 
+  try{
+    console.log('hiiiiiiii',req.model);
+    const data = await req.model.get();
+    console.log('get',data);
+    res.json(data);
+  } catch(error){
+    next(error.message);
+  }
+}
+
+//-----------------getByIdHandler-------------------------------
+async function getByIdHandler(req,res,next) {
+  const id=res.params.id;
+  try{
+    const data = await req.model.get(id);
+    res.json(data);
+  } catch(error){
+    next(error.message);
+  }
+}
+
+//-----------------postHandler-------------------------------
+async function postHandler(req,res,next) {
+  try{
+    const data = await req.model.create(res.body);
+    res.json(data);
+  } catch(error){
+    next(error.message);
+  }
+}
+
+//-----------------updateByIdHandler-------------------------------
+async function updateByIdHandler(req,res,next) {
+  const id=req.params.id;
+  try{
+    const data = await req.model.update(id,req.body);
+    res.json(data);
+  } catch(error){
+    next(error.message);
+  }
+}
+  
+//-----------------deleteHandler-------------------------------
+async function deleteHandler(req,res,next) {
+  const id=req.params.id;
+  try{
+    const data = await req.model.delete(id);
+    res.json(data);
+  } catch(error){
+    next(error.message);
+  }
+}
+
+module.exports=router;
+
